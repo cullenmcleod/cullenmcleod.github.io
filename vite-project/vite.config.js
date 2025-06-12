1f854533b373e49fa3be8444e91b2e2ffd90e3a8
cullenmcleod <merr.cully@outlook.com>
2025-06-12 18:10:34 +1200

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/cullenmcleod.github.io"
+  base: "/cullenmcleod.github.io.vite-project"
 
 })
